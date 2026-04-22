145098f2e07a4e1ad07076ccc0e0848801e16e8f
gsam404 <samanthaangelagalan@gmail.com>
2026-04-22 12:13:21 +0800

    feat: fixed entry logs table + log in/log out display

--- OTHERS/libgate_api/admin_login.php
+++ OTHERS/libgate_api/admin_login.php
@@ -1,5 +1,5 @@
 <?php
-// 1. Headers MUST come before any other code
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
